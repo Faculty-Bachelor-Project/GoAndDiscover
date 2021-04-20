6a5249f9fa73e7d1ee91ab415327bcf4216ec588
andreiz98 <61731341+andreiz98@users.noreply.github.com>
2021-04-20 21:53:47 +0300

    Remember button for login

--- GoDiscover/app/src/main/java/com/example/godiscover/login.java
+++ GoDiscover/app/src/main/java/com/example/godiscover/login.java
@@ -3,11 +3,14 @@ package com.example.godiscover;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -20,6 +23,28 @@ public class login extends AppCompatActivity {
     EditText password;
     ProgressBar progressBar;
     Button loginBt, signupButton, forgotBt;
+    CheckBox remember;
+    String Username;
+    String Password;
+
+    String prefs_name = "PrefsFile";
+    SharedPreferences MPrefs;
+
+    private void getPreferncesData() {
+        SharedPreferences SP = getSharedPreferences(prefs_name, MODE_PRIVATE);
+        if (SP.contains("pref_name")) {
+            String U = SP.getString("pref_name", "not found");
+            username.setText(U.toString());
+        }
+        if (SP.contains("pref_pass")) {
+            String P = SP.getString("pref_pass", "not found");
+            password.setText(P.toString());
+        }
+        if (SP.contains("pref_check")) {
+            Boolean b = SP.getBoolean("pref_check", false);
+            remember.setChecked(b);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +57,29 @@ public class login extends AppCompatActivity {
         loginBt = (Button) findViewById(R.id.loginBtn);
         signupButton = (Button) findViewById(R.id.signupBtn);
         forgotBt = (Button) findViewById(R.id.forgotBtn);
+        remember = (CheckBox)findViewById(R.id.RememberMe);
+
+        MPrefs = getSharedPreferences(prefs_name, MODE_PRIVATE);
+        getPreferncesData();
 
         loginBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Username = username.getText().toString();
-                String Password = password.getText().toString();
+                Username = username.getText().toString();
+                Password = password.getText().toString();
 
                 if(!Username.equals("") && !Password.equals("")) {
+
+                    if (remember.isChecked()) {
+                        Boolean bool_check = remember.isChecked();
+                        SharedPreferences.Editor editor = MPrefs.edit();
+                        editor.putString("pref_name", Username);
+                        editor.putString("pref_pass", Password);
+                        editor.putBoolean("pref_check", bool_check);
+                        editor.apply();
+                    }
+                    else MPrefs.edit().clear().apply();
+
                     progressBar.setVisibility(View.VISIBLE);
                     Handler handler = new Handler();
                     handler.post(new Runnable() {
@@ -54,7 +94,7 @@ public class login extends AppCompatActivity {
                             data[0] = Username;
                             data[1] = Password;
 
-                            PutData putData = new PutData("http://192.168.0.178/gndApp/login.php", "POST", field, data);
+                            PutData putData = new PutData("http://192.168.2.113/gndApp/login.php", "POST", field, data);
 
                             if (putData.startPut()) {
                                 if (putData.onComplete()) {
@@ -64,6 +104,8 @@ public class login extends AppCompatActivity {
                                         Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
                                         Intent intent = new Intent(login.this, img_processing.class);
                                         startActivity(intent);
+                                        username.getText().clear();
+                                        password.getText().clear();
                                         finish();
                                     } else
                                         Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
